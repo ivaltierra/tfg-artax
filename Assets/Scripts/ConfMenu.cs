7dd4808745a2605b937dc58dd2657730059e86c9
ivaltierra <ivaltierra@uoc.edu>
2020-04-04 22:16:37 +0200

    versión 1 diseño de transiciones de pantallas

---
+++ Assets/Scripts/ConfMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ConfMenu : MonoBehaviour{
+
+    public AudioMixer audioMixer;
+
+    public void AsignarVolumen(float volumen) {
+        audioMixer.SetFloat("VolumenMixer", volumen);
+
+    }
+}
---
+++ Assets/Scripts/esperar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class esperar : MonoBehaviour
+{
+    public float t_espera = 8f;
+    public int nScene = 1;
+    void Start()
+    {
+        StartCoroutine(esperarIntro());        
+    }
+
+    IEnumerator esperarIntro()
+    {
+       yield return new WaitForSeconds(t_espera);
+       SceneManager.LoadScene(nScene); 
+    }
+}
